496dda97a0a680d8ac6e36701200af02b20b535d
Jie Huang <huangjie@s152.uppmax.uu.se>
2022-10-14 19:24:54 +0200

    adapt the code to snowy

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-export BASE_PATH="/Users/taohuadao/Downloads/MtProject"
+export BASE_PATH="/home/huangjie/MtProject"
 cd ${BASE_PATH}
 eval "$(conda shell.bash hook)"
-conda activate daily
-nohup python run.py -c config.json -t train > ./outputs/run_history/baseline.out 2>&1 &
+conda activate py38_apex
+nohup python3.8 -u -m torch.distributed.launch --nproc_per_node=1 --master_port=59006 run.py \
+-c config.json -t train > ./outputs/run_history/baseline.out 2>&1 &
+#nohup python run.py -c config.json -t train > ./outputs/run_history/baseline.out 2>&1 &
     #-u -m torch.distributed.launch --nproc_per_node=4 --master_port=59006 run.py \
