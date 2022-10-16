d9e90f4178a3994a4d8da3e335dd91c0787b2572
jiejiejj <is.jiehuang@gmail.com>
2022-10-16 19:46:30 +0800

    save model after each epoch

--- run.py
+++ run.py
@@ -180,6 +180,8 @@ def train(model, optimizer, train_loader, dev_loader, epochs=1):
 
             if cfg['is_distributed']: torch.distributed.barrier()
             cur_step += 1
+        # epoch end
+        save_model(cfg, logger, cur_step, model, optimizer, lr_scheduler)
 
     if cfg['local_rank'] == 0:
         wandb.finish()
